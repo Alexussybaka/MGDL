3fd462221dff39c64429b55d365cabc5b0f641df
Alexussybaka <alex10.svoboda@gmail.com>
2025-01-17 19:45:18 +0100

    Coordinates navigation added

--- Assets/-Scripts/Pathfinder_2.cs
+++ Assets/-Scripts/Pathfinder_2.cs
@@ -13,8 +13,8 @@ public class Pathfinder_2 : MonoBehaviour
     private void Update()
     {
         //Drawing target coordinates
-        Debug.DrawLine(new Vector3(x_coordinates, 0f, 0f), new Vector3(-x_coordinates, 0f, 0f));
-        Debug.DrawLine(new Vector3(0f, 0f, -y_coordinates), new Vector3(-0f, 0f, y_coordinates));
+        Debug.DrawLine(new Vector3(x_coordinates - 1, 0f, y_coordinates), new Vector3(x_coordinates + 1, 0f, y_coordinates));
+        Debug.DrawLine(new Vector3(x_coordinates, 0f, y_coordinates - 1), new Vector3(x_coordinates, 0f, y_coordinates + 1));
 
         //Making move each time elapsed time variable hits period timing
         elapsed_time += Time.deltaTime;
@@ -43,7 +43,7 @@ public class Pathfinder_2 : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 transform.position += transform.forward;
             }
-            if(transform.position.x > x_coordinates)
+            if(transform.position.z > y_coordinates)
             {
                 transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 transform.position += transform.forward;
