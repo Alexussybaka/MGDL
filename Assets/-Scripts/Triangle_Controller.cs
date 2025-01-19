0d61de8032fb2f4e6bf30cb8fe0bb93e09a89d22
Alexussybaka <alex10.svoboda@gmail.com>
2025-01-19 12:35:22 +0100

    Added some functionallity.

--- Assets/-Scripts/Triangle_Controller.cs
+++ Assets/-Scripts/Triangle_Controller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Triangle_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float triangle_a, triangle_b, triangle_c;
+    [SerializeField] bool unknown_a, unknown_b, unknown_c;
+    [SerializeField] float triangle_area;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //Calculating area using a * b / 2 formula.
+        triangle_area = (triangle_a * triangle_b) / 2;
+
+        //Calculating unknown side using a^2 + b^2 = c^2.
+        if (unknown_a) triangle_a = Mathf.Sqrt((triangle_c * triangle_c) - (triangle_b * triangle_b));
+        if (unknown_b) triangle_b = Mathf.Sqrt((triangle_c * triangle_c) - (triangle_a * triangle_a));
+        if (unknown_c) triangle_c = Mathf.Sqrt((triangle_a * triangle_a) + (triangle_b * triangle_b));
+
+        // Drawing the triangle.
+
+        // Side a
+        Debug.DrawLine(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, triangle_a));
         
+        // Side b
+        Debug.DrawLine(new Vector3(0f, 0f, 0f), new Vector3(triangle_b, 0f, 0f));
+        
+        // Side c
+        Debug.DrawLine(new Vector3(triangle_b, 0f, 0f), new Vector3(0f, 0f, triangle_a));
     }
 }
