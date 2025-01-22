e49fc6630e4939cb3467ad7ac2bc6a8331eb76e8
Alexussybaka <alex10.svoboda@gmail.com>
2025-01-22 22:50:13 +0100

    Prework for later plans

--- Assets/-Scripts/Circle_Controller.cs
+++ Assets/-Scripts/Circle_Controller.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Circle_Controller : MonoBehaviour
 {
-    
 
 
+    [Range(1, 360)]
     [SerializeField] int subdivisions_count;
     [SerializeField] float radius;
     [SerializeField] Vector3 center;
@@ -14,20 +14,23 @@ public class Circle_Controller : MonoBehaviour
     
     [SerializeField] float area;
 
-    private Vector3[] rotated_points;
+    private List<Vector3> rotated_points;
 
     private void Update()
     {
         float angle = (360 / subdivisions_count) / (180 / Mathf.PI);
 
-        for (int i = 0; i < subdivisions_count+1; i++)
+        for (int i = 1; i < subdivisions_count+1; i++)
         {
+            // Safe procedures to ensure we don't get zero division error.
+            if(subdivisions_count == 0) return;
 
-            float x_pos = radius * Mathf.Cos(angle);
-            float y_pos = radius* Mathf.Sin(angle);
+
+            float x_pos = radius * Mathf.Cos(angle * i);
+            float y_pos = radius* Mathf.Sin(angle * i);
 
             Vector3 rotation_point = new Vector3(x_pos, 0f, y_pos);
-            rotated_points[i] = rotation_point;
+            rotated_points.Add(rotation_point);
 
             Debug.DrawLine(center, rotation_point);
         }
