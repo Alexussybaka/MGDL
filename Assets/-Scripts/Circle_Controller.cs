c11b00a647300636cbe570405c443a1503bb61d5
Alexussybaka <alex10.svoboda@gmail.com>
2025-01-22 22:06:11 +0100

    Created points position calculation

--- Assets/-Scripts/Circle_Controller.cs
+++ Assets/-Scripts/Circle_Controller.cs
@@ -14,13 +14,22 @@ public class Circle_Controller : MonoBehaviour
     
     [SerializeField] float area;
 
+    private Vector3[] rotated_points;
+
     private void Update()
     {
-        float angle = 360 / subdivisions_count;
+        float angle = (360 / subdivisions_count) / (180 / Mathf.PI);
 
-        for (int i = 1; i < subdivisions_count+1; i++)
+        for (int i = 0; i < subdivisions_count+1; i++)
         {
 
+            float x_pos = radius * Mathf.Cos(angle);
+            float y_pos = radius* Mathf.Sin(angle);
+
+            Vector3 rotation_point = new Vector3(x_pos, 0f, y_pos);
+            rotated_points[i] = rotation_point;
+
+            Debug.DrawLine(center, rotation_point);
         }
     }
 }
