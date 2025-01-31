0e158ec85475b9fb58f25968ce783bbca9d27985
Alexussybaka <alex10.svoboda@gmail.com>
2025-01-31 13:12:20 +0100

    Base for Sqrt and Power visualisation calculator.

---
+++ Assets/-Scripts/SquareRoot_And_Power_Calculator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareRoot_And_Power_Calculator : MonoBehaviour
+{
+    [SerializeField] float resolution;
+    [SerializeField] float aproxximate_limit;
+    [SerializeField] bool horizontal;
+
+    public void Update()
+    {
+        for (float i = 0; i < aproxximate_limit; i+= resolution)
+        {
+            if (horizontal)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
