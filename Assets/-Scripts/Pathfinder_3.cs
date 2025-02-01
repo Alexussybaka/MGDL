4e7849fd737ad5e66c9440c39f8a40f5618f08ce
Alexussybaka <alex10.svoboda@gmail.com>
2025-02-01 20:45:11 +0100

    Pathfinder bug fixing

--- Assets/-Scripts/Pathfinder_3.cs
+++ Assets/-Scripts/Pathfinder_3.cs
@@ -6,18 +6,31 @@ using UnityEngine;
 public class Pathfinder_3 : MonoBehaviour
 {
     [Header("Properties")]
-    [SerializeField] private List<Coordinate> coordinates;
+    [SerializeField] private List<Vector2> coordinates;
+    [SerializeField] private bool create_random_points;
+    [Space]
     [SerializeField] private int current_destination = 0;
     [SerializeField] private float period;
-    
+
     private float elapsed_time;
+
+    private void Start()
+    {
+        if (create_random_points)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                coordinates.Add(new Vector2(UnityEngine.Random.Range(-20, 20), UnityEngine.Random.Range(-20, 20)));
+            }
+        }
+    }
 
     private void Update()
     {
-        foreach (var c in coordinates)
+        foreach (Vector2 coordinate in coordinates)
         {
-            Debug.DrawLine(new Vector3(c.coordinate.x - 1, 0f, c.coordinate.y), new Vector3(c.coordinate.x + 1, 0f, c.coordinate.y));
-            Debug.DrawLine(new Vector3(c.coordinate.x, 0f, c.coordinate.y - 1), new Vector3(c.coordinate.x, 0f, c.coordinate.y + 1));
+            Debug.DrawLine(new Vector3(coordinate.x - 1, 0f, coordinate.y), new Vector3(coordinate.x + 1, 0f, coordinate.y));
+            Debug.DrawLine(new Vector3(coordinate.x, 0f, coordinate.y - 1), new Vector3(coordinate.x, 0f, coordinate.y + 1));
         }
 
         elapsed_time += Time.deltaTime;
@@ -29,24 +42,24 @@ public class Pathfinder_3 : MonoBehaviour
     }
     public void MakeMove()
     {
-        if (Vector3.Distance(transform.position, new Vector3(coordinates[current_destination].coordinate.x, 0f, coordinates[current_destination].coordinate.x)) > 0.1f)
+        if (Vector3.Distance(transform.position, new Vector3(coordinates[current_destination].x, 0f, coordinates[current_destination].y)) > 1f)
         {
-            if (transform.position.x < coordinates[current_destination].coordinate.x)
+            if (transform.position.x < coordinates[current_destination].x)
             {
                 transform.rotation = Quaternion.Euler(0f, 90f, 0f);
                 transform.position += transform.forward;
             }
-            if (transform.position.x > coordinates[current_destination].coordinate.x)
+            if (transform.position.x > coordinates[current_destination].x)
             {
                 transform.rotation = Quaternion.Euler(0f, 270f, 0f);
                 transform.position += transform.forward;
             }
-            if (transform.position.z < coordinates[current_destination].coordinate.y)
+            if (transform.position.z < coordinates[current_destination].y)
             {
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 transform.position += transform.forward;
             }
-            if (transform.position.z > coordinates[current_destination].coordinate.y)
+            if (transform.position.z > coordinates[current_destination].y)
             {
                 transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 transform.position += transform.forward;
@@ -61,10 +74,4 @@ public class Pathfinder_3 : MonoBehaviour
         }
 
     }
-}
-
-[System.Serializable]
-public class Coordinate
-{
-    public Vector2 coordinate;
 }
