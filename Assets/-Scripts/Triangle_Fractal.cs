d5c61c1720b7154a24ec2b8af1ba67dcbd8f5936
Alexussybaka <alex10.svoboda@gmail.com>
2025-02-08 19:00:15 +0100

    First stage rendering

--- Assets/-Scripts/Triangle_Fractal.cs
+++ Assets/-Scripts/Triangle_Fractal.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Triangle_Fractal : MonoBehaviour
 {
-    
+    [SerializeField] int stage;
+    [SerializeField] float scale;
+
+    private void Update()
+    {
+        Vector3 high_point = new Vector3(0f, 0f, Mathf.Sqrt(Mathf.Pow(scale, 2) - Mathf.Pow(scale / 2, 2)));
+
+        Debug.DrawLine(new Vector3(-(scale / 2), 0f, 0f), new Vector3(scale / 2, 0f, 0f));
+        Debug.DrawLine(new Vector3(-(scale / 2), 0f, 0f), high_point);
+        Debug.DrawLine(new Vector3(scale / 2, 0f, 0f), high_point);
+    }
 }
