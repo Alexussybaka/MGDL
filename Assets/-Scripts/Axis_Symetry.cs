b36415cfb4ad9ee2482f8195896a64292f2a0096
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-18 17:47:17 +0200

    Axis symetry script - started work

---
+++ Assets/-Scripts/Axis_Symetry.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis_Symetry : MonoBehaviour
+{
+    [Header("Axis Parameters")]
+    [SerializeField] private Vector2 intersecting_x;
+    [SerializeField] private Vector2 intersecting_y;
+}
