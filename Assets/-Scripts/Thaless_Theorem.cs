5cda5cc096e64e0dbba3a1ac0378211a856ac4ef
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-20 11:06:14 +0200

    Drawing circle function

--- Assets/-Scripts/Thaless_Theorem.cs
+++ Assets/-Scripts/Thaless_Theorem.cs
@@ -9,6 +9,9 @@ public class Thaless_Theorem : MonoBehaviour
     [Range(0f, 360f)]
     [SerializeField] private float angle;
 
+    [SerializeField] private bool draw_circle;
+    private List<Vector3> rotated_points;
+
     private void Update()
     {
         Debug.DrawLine(new Vector3(-radius, 0f, 0f), new Vector3(radius, 0f, 0f));
@@ -17,5 +20,28 @@ public class Thaless_Theorem : MonoBehaviour
 
         Debug.DrawLine(new Vector3(radius, 0f, 0f), right_point);
         Debug.DrawLine(new Vector3(-radius, 0f, 0f), right_point);
+
+        //Drawing circle
+        if (draw_circle)
+        {
+            float angle = (360 / 24) / (180 / Mathf.PI);
+
+            for (int i = 1; i < 24 + 1; i++)
+            {
+                float x_pos = radius * Mathf.Cos(angle * i);
+                float y_pos = radius * Mathf.Sin(angle * i);
+
+                Vector3 rotation_point = new Vector3(x_pos, 0f, y_pos);
+                rotated_points.Add(rotation_point);
+            }
+
+            for (int i = 0; i < rotated_points.Count - 1; i++)
+            {
+                Debug.DrawLine(rotated_points[i], rotated_points[i + 1]);
+            }
+            Debug.DrawLine(rotated_points[rotated_points.Count - 1], rotated_points[0]);
+
+            rotated_points.Clear();
+        }
     }
 }
