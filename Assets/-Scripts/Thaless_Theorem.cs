a0a6d55f01691f155bd717d84bedebfdde76733a
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-20 14:08:21 +0200

    Thaless Theorem : DONE

--- Assets/-Scripts/Thaless_Theorem.cs
+++ Assets/-Scripts/Thaless_Theorem.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Thaless_Theorem : MonoBehaviour
 {
-    [SerializeField] private float radius;
-
+    [Header("Parameters")]
     [Range(0f, 360f)]
     [SerializeField] private float angle;
-
+    [Space]
+    [SerializeField] private float radius;
+    [Space]
     [SerializeField] private bool draw_circle;
+    [Range(1f, 360f)]
+    [SerializeField] int subdivision_count;
     private List<Vector3> rotated_points = new List<Vector3>();
 
     private void Update()
     {
+        //Drawing hypotenuse
         Debug.DrawLine(new Vector3(-radius, 0f, 0f), new Vector3(radius, 0f, 0f));
 
+        //Drawing right angle point
         Vector3 right_point = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)) * radius, 0f, Mathf.Sin(angle * (Mathf.PI / 180f)) * radius);
 
         Debug.DrawLine(new Vector3(radius, 0f, 0f), right_point);
@@ -24,9 +29,9 @@ public class Thaless_Theorem : MonoBehaviour
         //Drawing circle
         if (draw_circle)
         {
-            float angle = (360 / 24) / (180 / Mathf.PI);
+            float angle = (360 / subdivision_count) / (180 / Mathf.PI);
 
-            for (int i = 1; i < 25; i++)
+            for (int i = 1; i < subdivision_count + 1; i++)
             {
                 float x_pos = radius * Mathf.Cos(angle * i);
                 float y_pos = radius * Mathf.Sin(angle * i);
