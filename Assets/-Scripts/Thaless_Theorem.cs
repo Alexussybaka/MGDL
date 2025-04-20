9727fd54c394c1d025c1e31d788883ed96dcee55
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-20 10:59:25 +0200

    First prototype

---
+++ Assets/-Scripts/Thaless_Theorem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thaless_Theorem : MonoBehaviour
+{
+    [SerializeField] private float radius;
+
+    [Range(0f, 360f)]
+    [SerializeField] private float angle;
+
+    private void Update()
+    {
+        Debug.DrawLine(new Vector3(-radius, 0f, 0f), new Vector3(radius, 0f, 0f));
+
+        Vector3 right_point = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)), 0f, Mathf.Sin(angle * (Mathf.PI / 180f)));
+    }
+}
