0e8572ab4ae3f9dbf5f785c275d2efb01a6013a1
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-20 14:02:25 +0200

    Another bug fixing, seems like it is done

--- Assets/-Scripts/Thaless_Theorem.cs
+++ Assets/-Scripts/Thaless_Theorem.cs
@@ -10,13 +10,13 @@ public class Thaless_Theorem : MonoBehaviour
     [SerializeField] private float angle;
 
     [SerializeField] private bool draw_circle;
-    private List<Vector3> rotated_points;
+    private List<Vector3> rotated_points = new List<Vector3>();
 
     private void Update()
     {
         Debug.DrawLine(new Vector3(-radius, 0f, 0f), new Vector3(radius, 0f, 0f));
 
-        Vector3 right_point = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)), 0f, Mathf.Sin(angle * (Mathf.PI / 180f)));
+        Vector3 right_point = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)) * radius, 0f, Mathf.Sin(angle * (Mathf.PI / 180f)) * radius);
 
         Debug.DrawLine(new Vector3(radius, 0f, 0f), right_point);
         Debug.DrawLine(new Vector3(-radius, 0f, 0f), right_point);
