43a48a322fb5287c1241e3d37d4c921711a37495
Alexussybaka <alex10.svoboda@gmail.com>
2025-04-29 21:48:48 +0200

    Drawing axis

--- Assets/-Scripts/Axis_Symetry.cs
+++ Assets/-Scripts/Axis_Symetry.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Axis_Symetry : MonoBehaviour
 {
     [Header("Axis Parameters")]
-    [SerializeField] private Vector2 intersecting_x;
-    [SerializeField] private Vector2 intersecting_y;
+    [Range(0f, 100f)]
+    [SerializeField] private float axis_view_multiplier;
+    [SerializeField] private Vector2 axis_vector;
+
+    private void Update()
+    {
+        Debug.DrawLine(Vector3.zero, new Vector3(axis_vector.x, 0f, axis_vector.y) * axis_view_multiplier);
+        Debug.DrawLine(Vector3.zero, -new Vector3(axis_vector.x, 0f, axis_vector.y) * axis_view_multiplier);
+    }
 }
