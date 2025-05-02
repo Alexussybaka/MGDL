3dcf19e2a4b7cae63b423b6e83b4daf7393c4692
Alexussybaka <alex10.svoboda@gmail.com>
2025-05-02 13:01:38 +0200

    Almost done

--- Assets/-Scripts/Axis_Symetry.cs
+++ Assets/-Scripts/Axis_Symetry.cs
@@ -22,12 +22,24 @@ public class Axis_Symetry : MonoBehaviour
             Debug.DrawLine(new Vector3(vertexes[i].x, 0f, vertexes[i].y), new Vector3(vertexes[i + 1].x, 0f, vertexes[i + 1].y));
         }
 
-        Vector3 second_vector = new Vector3(axis_vector.y, 0f, -axis_vector.x);
-        Debug.DrawLine(Vector3.zero, second_vector);
+        for (int i = 0;i < vertexes.Count - 1; i++)
+        {
+            //formula = float det = ((vertexes[i].x - (axis_vector.y + vertexes[i].x)) * (0 - axis_vector.y)) - ((vertexes[i].y - (-axis_vector.x + vertexes[i].y)) * (0 - axis_vector.x));
+            // Simplified to:
+            float det = Mathf.Pow(axis_vector.y, 2) + Mathf.Pow(axis_vector.x, 2);
 
-        //for (int i = 0; i < vertexes.Count - 1; i++)
-        //{
+            if (det == 0) return;
+            else
+            {
+                // X
+                float midle_x = ((Mathf.Pow(axis_vector.x, 2) * vertexes[i].x) + (axis_vector.y * vertexes[i].y * axis_vector.x)) / det;
 
-        //}
+                // Y
+                float midle_y = ((axis_vector.x * vertexes[i].x * axis_vector.y) + (Mathf.Pow(axis_vector.y, 2) * vertexes[i].y)) / det;
+
+                Vector3 midle_point = new Vector3();
+            }
+        }
+        
     }
 }
