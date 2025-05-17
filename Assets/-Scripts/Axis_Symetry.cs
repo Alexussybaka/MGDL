ada400220426f62e23c65bb6c3c4180607133844
Alexussybaka <alex10.svoboda@gmail.com>
2025-05-17 13:23:35 +0200

    Some little progress on axis symetry

--- Assets/-Scripts/Axis_Symetry.cs
+++ Assets/-Scripts/Axis_Symetry.cs
@@ -10,10 +10,18 @@ public class Axis_Symetry : MonoBehaviour
     [SerializeField] private Vector2 axis_vector;
     [SerializeField] private List<Vector2> vertexes;
 
-    private List<Vector3> picture = new List<Vector3>();
+    private List<Vector2> picture = new List<Vector2>();
 
     private void Update()
     {
+        //Calculating right-angle vector
+        Vector2 right_vector = new Vector2(
+            axis_vector.y,
+            -axis_vector.x
+        );
+
+        right_vector.Normalize();
+
         //Drawing axis
         Debug.DrawLine(Vector3.zero, new Vector3(axis_vector.x, 0f, axis_vector.y) * axis_view_multiplier);
         Debug.DrawLine(Vector3.zero, -new Vector3(axis_vector.x, 0f, axis_vector.y) * axis_view_multiplier);
@@ -27,27 +35,20 @@ public class Axis_Symetry : MonoBehaviour
         //Computing new picture point
         for (int i = 0;i < vertexes.Count - 1; i++)
         {
-            //formula = float det = ((vertexes[i].x - (axis_vector.y + vertexes[i].x)) * (0 - axis_vector.y)) - ((vertexes[i].y - (-axis_vector.x + vertexes[i].y)) * (0 - axis_vector.x));
-            // Simplified to:
-            float det = Mathf.Pow(axis_vector.y, 2) + Mathf.Pow(axis_vector.x, 2);
+            float distance = Mathf.Abs(axis_vector.y * vertexes[i].x - axis_vector.x * vertexes[i].y) / Mathf.Sqrt(Mathf.Pow(axis_vector.y, 2) + Mathf.Pow(axis_vector.x, 2));
 
-            if (det == 0) return;
-            else
-            {
-                // X
-                float middle_x = ((Mathf.Pow(axis_vector.x, 2) * vertexes[i].x) + (axis_vector.y * vertexes[i].y * axis_vector.x)) / det;
+            Vector2 picture_point = new Vector2(
+                vertexes[i].x * distance,
+                vertexes[i].y * distance
+            );
 
-                // Y
-                float middle_y = ((axis_vector.x * vertexes[i].x * axis_vector.y) + (Mathf.Pow(axis_vector.y, 2) * vertexes[i].y)) / det;
+            picture_point *= right_vector;
 
-                Vector3 picture_point = new Vector3((2 * middle_x) - vertexes[i].x, 0f, (2 * middle_y) - vertexes[i].y);
-
-                picture.Add(picture_point);
-            }
+            picture.Add(new Vector2(picture_point.x, picture_point.y));
         }
 
         // Rendering the picture
-        if (picture.Count > 0)
+        if (picture.Count > 1)
         {
             for (int i = 0; i < picture.Count - 1; i++)
             {
