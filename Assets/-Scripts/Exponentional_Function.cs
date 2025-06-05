dc89fe9108f37e731f904d4622655557984886fc
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-05 21:57:21 +0200

    Exponentional_Function.cs Final

--- Assets/-Scripts/Exponentional_Function.cs
+++ Assets/-Scripts/Exponentional_Function.cs
@@ -24,6 +24,8 @@ public class Exponentional_Function : MonoBehaviour
 
     private void Update()
     {
+        if (eulers) a = 2.71828f;
+
         for (float i = -limit; i < limit; i += resolution)
         {
             vectors.Add(new Vector3(i, 0f, Mathf.Pow(a, i)));
@@ -31,7 +33,36 @@ public class Exponentional_Function : MonoBehaviour
 
         for (int i = 0; i < vectors.Count - 1; i++)
         {
+            if (vectors[i].z > limit) continue;
             Debug.DrawLine(vectors[i], vectors[i + 1]);
+        }
+
+        vectors.Clear();
+
+        Visualise_Examined_Number();
+        ShowAxis();
+    }
+
+    private void Visualise_Examined_Number()
+    {
+        evaluation = Mathf.Pow(a, number);
+
+        if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.red);
+        else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.red);
+
+        if (evaluation >= 0) Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.blue);
+        else Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, 0.5f), Color.blue);
+
+        Debug.DrawLine(new Vector3(number, 0, 0), new Vector3(number, 0, evaluation), Color.green);
+        Debug.DrawLine(new Vector3(0, 0, evaluation), new Vector3(number, 0, evaluation), Color.green);
+    }
+
+    private void ShowAxis()
+    {
+        if (show_axis)
+        {
+            Debug.DrawLine(new Vector3(-limit, 0, 0), new Vector3(limit, 0, 0));
+            Debug.DrawLine(new Vector3(0, 0, -limit), new Vector3(0, 0, limit));
         }
     }
 }
