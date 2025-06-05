6dc9ad69c4f72ea620ce69303eeb98acd72502cb
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-05 21:38:09 +0200

    Update Exponentional_Function.cs

--- Assets/-Scripts/Exponentional_Function.cs
+++ Assets/-Scripts/Exponentional_Function.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Exponentional_Function : MonoBehaviour
 {
     [Header("Graph Settings")]
-    [Header("y = log(a, x)")]
+    [Header("y = a^x")]
     [SerializeField] float a;
     [SerializeField] bool eulers;
     [Space]
@@ -21,4 +21,17 @@ public class Exponentional_Function : MonoBehaviour
     [SerializeField] float evaluation;
 
     private List<Vector3> vectors = new List<Vector3>();
+
+    private void Update()
+    {
+        for (float i = -limit; i < limit; i += resolution)
+        {
+            vectors.Add(new Vector3(i, 0f, Mathf.Pow(a, i)));
+        }
+
+        for (int i = 0; i < vectors.Count - 1; i++)
+        {
+            Debug.DrawLine(vectors[i], vectors[i + 1]);
+        }
+    }
 }
