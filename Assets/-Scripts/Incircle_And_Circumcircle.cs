9640095be06d7ba0e08dd446f486143d472519ce
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-06 23:43:25 +0200

    Update Incircle_And_Circumcircle.cs

--- Assets/-Scripts/Incircle_And_Circumcircle.cs
+++ Assets/-Scripts/Incircle_And_Circumcircle.cs
@@ -7,7 +7,12 @@ public class Incircle_And_Circumcircle : MonoBehaviour
     [SerializeField] Vector2 A;
     [SerializeField] Vector2 B;
     [SerializeField] Vector2 C;
-    [SerializeField] Vector3 center;
+    
+    [SerializeField] Vector3 in_center;
+    [SerializeField] float in_radius;
+    
+    [SerializeField] Vector3 ci_center;
+    [SerializeField] float ci_radius;
 
     private void Update()
     {
