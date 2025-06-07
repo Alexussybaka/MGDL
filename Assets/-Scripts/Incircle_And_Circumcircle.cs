73cbb95bb8051d481cacafa3ff50a029bf779916
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-07 15:45:47 +0200

    Update Incircle_And_Circumcircle.cs

--- Assets/-Scripts/Incircle_And_Circumcircle.cs
+++ Assets/-Scripts/Incircle_And_Circumcircle.cs
@@ -7,7 +7,8 @@ public class Incircle_And_Circumcircle : MonoBehaviour
     [SerializeField] Vector2 A;
     [SerializeField] Vector2 B;
     [SerializeField] Vector2 C;
-    
+
+    [Space]
     [SerializeField] Vector3 in_center;
     [SerializeField] float in_radius;
     
@@ -21,8 +22,20 @@ public class Incircle_And_Circumcircle : MonoBehaviour
         Debug.DrawLine(new Vector3(A.x, 0f, A.y), new Vector3(C.x, 0f, C.y));
         Debug.DrawLine(new Vector3(C.x, 0f, C.y), new Vector3(B.x, 0f, B.y));
 
-        Debug.DrawLine(new Vector3(A.x, 0f, A.y), new Vector3((B.x + C.x) / 2, 0f, (B.y + C.y) / 2));
-        Debug.DrawLine(new Vector3(B.x, 0f, B.y), new Vector3((A.x + C.x) / 2, 0f, (A.y + C.y) / 2));
-        Debug.DrawLine(new Vector3(C.x, 0f, C.y), new Vector3((B.x + A.x) / 2, 0f, (B.y + A.y) / 2));
+        //Debug.DrawLine(new Vector3(A.x, 0f, A.y), new Vector3((B.x + C.x) / 2, 0f, (B.y + C.y) / 2));
+        //Debug.DrawLine(new Vector3(B.x, 0f, B.y), new Vector3((A.x + C.x) / 2, 0f, (A.y + C.y) / 2));
+        //Debug.DrawLine(new Vector3(C.x, 0f, C.y), new Vector3((B.x + A.x) / 2, 0f, (B.y + A.y) / 2));
+
+
+        //Circumcircle : Calculation
+        float d = 2 * ((A.x * (B.y - C.y)) + (B.x * (C.y - A.y)) + (C.x * (A.y - B.y)));
+
+        ci_center.x = (1 / d) * ((((A.x * A.x) + (A.y * A.y)) * (B.y - C.y)) + (((B.x * B.x) + (B.y * B.y)) * (C.y - A.y)) + (((C.x * C.x) + (C.y * C.y)) * (A.y - B.y)));
+        ci_center.y = (1 / d) * ((((A.x * A.x) + (A.y * A.y)) * (C.x - B.x)) + (((B.x * B.x) + (B.y * B.y)) * (A.x - C.x)) + (((C.x * C.x) + (C.y * C.y)) * (B.x - A.x)));
+
+        ci_radius = Vector3.Distance(ci_center, A);
+
+        //Circumcircle : Rendering
+
     }
 }
