a7be12e41f08ca11288ce581abb43a94843c1a65
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-07 19:19:18 +0200

    Update Incircle_And_Circumcircle.cs

--- Assets/-Scripts/Incircle_And_Circumcircle.cs
+++ Assets/-Scripts/Incircle_And_Circumcircle.cs
@@ -14,6 +14,7 @@ public class Incircle_And_Circumcircle : MonoBehaviour
     [SerializeField] int in_subdivision_count;
     private List<Vector3> in_rotated_points = new List<Vector3>();
 
+    [Space]
     [SerializeField] Vector3 ci_center;
     [SerializeField] float ci_radius;
     [SerializeField] int ci_subdivision_count;
@@ -30,17 +31,54 @@ public class Incircle_And_Circumcircle : MonoBehaviour
         //Debug.DrawLine(new Vector3(B.x, 0f, B.y), new Vector3((A.x + C.x) / 2, 0f, (A.y + C.y) / 2));
         //Debug.DrawLine(new Vector3(C.x, 0f, C.y), new Vector3((B.x + A.x) / 2, 0f, (B.y + A.y) / 2));
 
+        //Incircle : Calculation
+        float a = Vector3.Distance(B, C);
+        float b = Vector3.Distance(A, C);
+        float c = Vector3.Distance(A, B);
+
+        in_center.x = ((a * A.x) + (b * B.x) + (c * C.x)) / (a + b + c);
+        in_center.y = 0;
+        in_center.z = ((a * A.y) + (b * B.y) + (c * C.y)) / (a + b + c);
+
+        float s = 0.5f * (a + b + c);
+        in_radius = Mathf.Sqrt(((s - a) * (s - b) * (s - c)) / s);
+
+
+        //Incircle : Rendering
+        float angle = (360 / in_subdivision_count) / (180 / Mathf.PI);
+
+        for (int i = 1; i < in_subdivision_count + 1; i++)
+        {
+            float x_pos = in_radius * Mathf.Cos(angle * i);
+            float y_pos = in_radius * Mathf.Sin(angle * i);
+
+            Vector3 rotation_point = new Vector3(x_pos + in_center.x, 0f, y_pos + in_center.z);
+            in_rotated_points.Add(rotation_point);
+        }
+
+        for (int i = 0; i < in_rotated_points.Count - 1; i++)
+        {
+            Debug.DrawLine(in_rotated_points[i], in_rotated_points[i + 1]);
+        }
+        Debug.DrawLine(in_rotated_points[in_rotated_points.Count - 1], in_rotated_points[0]);
+
+        in_rotated_points.Clear();
+
+
+
 
         //Circumcircle : Calculation
         float d = 2 * ((A.x * (B.y - C.y)) + (B.x * (C.y - A.y)) + (C.x * (A.y - B.y)));
         
         ci_center.x = (1 / d) * ((((A.x * A.x) + (A.y * A.y)) * (B.y - C.y)) + (((B.x * B.x) + (B.y * B.y)) * (C.y - A.y)) + (((C.x * C.x) + (C.y * C.y)) * (A.y - B.y)));
+        ci_center.y = 0;
         ci_center.z = (1 / d) * ((((A.x * A.x) + (A.y * A.y)) * (C.x - B.x)) + (((B.x * B.x) + (B.y * B.y)) * (A.x - C.x)) + (((C.x * C.x) + (C.y * C.y)) * (B.x - A.x)));
 
-        ci_radius = Vector3.Distance(ci_center, A);
+        ci_radius = Vector3.Distance(ci_center, A); // A.y should always stay 0 for code to work propperly
+
 
         //Circumcircle : Rendering
-        float angle = (360 / ci_subdivision_count) / (180 / Mathf.PI);
+        angle = (360 / ci_subdivision_count) / (180 / Mathf.PI);
 
         for (int i = 1; i < ci_subdivision_count + 1; i++)
         {
