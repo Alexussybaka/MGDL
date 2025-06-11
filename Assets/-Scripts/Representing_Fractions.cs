56d9101d60f11145a55f2659d40106df814f11c7
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-11 19:20:43 +0200

    Update Representing_Fractions.cs

--- Assets/-Scripts/Representing_Fractions.cs
+++ Assets/-Scripts/Representing_Fractions.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 public class Representing_Fractions : MonoBehaviour
 {
     [SerializeField] bool via_circle;
+    [SerializeField] float fraction_value;
+    [SerializeField] float maximum_value;
     List<Vector3> rotated_points = new List<Vector3>();
 
     private void Update()
     {
-        float angle = 4 / (180 / Mathf.PI);
+        if (maximum_value == 0) maximum_value = 0.0000000001f;
 
-        for (int i = 1; i < 90 + 1; i++)
+        if (via_circle)
         {
-            float x_pos = 10 * Mathf.Cos(angle * i);
-            float y_pos = 10 * Mathf.Sin(angle * i);
+            float angle = 4 / (180 / Mathf.PI);
+            float radians_fraction = (fraction_value / maximum_value) * 2 * Mathf.PI;
 
-            Vector3 rotation_point = new Vector3(x_pos, 0f, y_pos);
-            rotated_points.Add(rotation_point);
-        }
+            for (int i = 1; i < 90 + 1; i++)
+            {
+                float x_pos = 10 * Mathf.Cos(angle * i);
+                float y_pos = 10 * Mathf.Sin(angle * i);
 
-        for (int i = 0; i < rotated_points.Count - 1; i++)
-        {
-            Debug.DrawLine(rotated_points[i], rotated_points[i + 1]);
+                Vector3 rotation_point = new Vector3(x_pos, 0f, y_pos);
+                rotated_points.Add(rotation_point);
+            }
+
+            for (int i = 0; i < rotated_points.Count - 1; i++)
+            {
+                Debug.DrawLine(rotated_points[i], rotated_points[i + 1]);
+            }
+            Debug.DrawLine(rotated_points[rotated_points.Count - 1], rotated_points[0]);
+
+            Debug.DrawLine(Vector3.zero, new Vector3(10, 0f, 0f));
+            Debug.DrawLine(Vector3.zero, new Vector3(Mathf.Cos(radians_fraction), 0f, Mathf.Sin(radians_fraction)));
         }
-        Debug.DrawLine(rotated_points[rotated_points.Count - 1], rotated_points[0]);
     }
 }
