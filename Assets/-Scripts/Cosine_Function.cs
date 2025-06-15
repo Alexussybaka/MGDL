d434f7ded45c1e81c1eb94e098a839df0d2035a3
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-15 19:08:59 +0200

    Cosine Function Start

---
+++ Assets/-Scripts/Cosine_Function.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cosine_Function : MonoBehaviour
+{
+    [Header("Graph Settings")]
+    [SerializeField] bool show_axis;
+    [SerializeField] float limit;
+    [Range(0.05f, 5f)]
+    [SerializeField] float resolution;
+
+    [Space]
+    [Header("Analyzed Number")]
+    [SerializeField] float number;
+    [SerializeField] bool radians;
+    [Space]
+    [SerializeField] float evaluation;
+}
