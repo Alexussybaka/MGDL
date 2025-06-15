30692acad696087264411cfcc167bda057b8d9ee
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-15 21:06:03 +0200

    Added Sine Function

---
+++ Assets/-Scripts/Calculus/Sine_Function.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sine_Function : MonoBehaviour
+{
+    [Header("Graph Settings")]
+    [SerializeField] bool show_axis;
+    [SerializeField] float limit;
+    [Range(0.05f, 5f)]
+    [SerializeField] float resolution;
+
+    [Space]
+    [Header("Analyzed Number")]
+    [SerializeField] float number;
+    [SerializeField] bool radians;
+    [Space]
+    [SerializeField] float evaluation;
+
+    private List<Vector3> vectors = new List<Vector3>();
+
+    private void Update()
+    {
+        if (limit < 0) limit = 0;
+
+        for (float i = -limit; i < limit; i += resolution)
+        {
+            vectors.Add(new Vector3(i, 0f, Mathf.Sin(i)));
+        }
+
+        for (int i = 0; i < vectors.Count - 1; i++)
+        {
+            Debug.DrawLine(vectors[i], vectors[i + 1]);
+        }
+
+        vectors.Clear();
+
+        Visualise_Examined_Number();
+        Show_Axis();
+    }
+
+    public void Visualise_Examined_Number()
+    {
+        if (radians)
+        {
+            evaluation = Mathf.Sin(number * Mathf.PI);
+
+            if (evaluation >= 0) Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, -0.5f), Color.red);
+            else Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, 0.5f), Color.red);
+
+            if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.blue);
+            else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.blue);
+
+            Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, evaluation), Color.green);
+            Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, evaluation), new Vector3(0f, 0f, evaluation), Color.green);
+        }
+        else
+        {
+            evaluation = Mathf.Sin(number);
+
+            if (evaluation >= 0) Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.red);
+            else Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, 0.5f), Color.red);
+
+            if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.blue);
+            else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.blue);
+
+            Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, evaluation), Color.green);
+            Debug.DrawLine(new Vector3(number, 0f, evaluation), new Vector3(0f, 0f, evaluation), Color.green);
+        }
+    }
+
+    public void Show_Axis()
+    {
+        if (show_axis)
+        {
+            Debug.DrawLine(new Vector3(-(limit * limit), 0, 0), new Vector3(limit * limit, 0, 0));
+            Debug.DrawLine(new Vector3(0, 0, -(limit * limit)), new Vector3(0, 0, limit * limit));
+        }
+    }
+}
