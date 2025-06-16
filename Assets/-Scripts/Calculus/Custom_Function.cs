4e1ec5c3603c8e304344bdf980841e19403a5520
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-16 17:55:42 +0200

    Custom Function Start

---
+++ Assets/-Scripts/Calculus/Custom_Function.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom_Function : MonoBehaviour
+{
+    [Header("Graph Settings")]
+    [SerializeField] string y_equals;
+    [Space]
+    [SerializeField] bool show_axis;
+    [SerializeField] float limit;
+    [Range(0.01f, 5f)]
+    [SerializeField] float resolution;
+
+    [Space]
+    [Header("Analyzed Number")]
+    [SerializeField] float number;
+    [SerializeField] bool radians;
+    [Space]
+    [SerializeField] float evaluation;
+
+    private List<Vector3> vectors = new List<Vector3>();
+
+    private void Update()
+    {
+        
+    }
+}
