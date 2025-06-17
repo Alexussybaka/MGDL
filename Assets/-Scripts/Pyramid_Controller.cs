04db8215f73424a579c1a3619b5c1997a7919e35
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-17 17:09:54 +0200

    Pyramid Controller + Project Architecture fix

---
+++ Assets/-Scripts/Pyramid_Controller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid_Controller : MonoBehaviour
+{
+    [Header("Pyramid Settings")]
+    [SerializeField] float height;
+    [SerializeField] float width;
+}
