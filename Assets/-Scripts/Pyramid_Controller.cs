36a5b2ddfb5c559ce8ce0961262803509aa8c4c7
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-17 17:19:52 +0200

    Update Pyramid_Controller.cs

--- Assets/-Scripts/Pyramid_Controller.cs
+++ Assets/-Scripts/Pyramid_Controller.cs
@@ -7,4 +7,26 @@ public class Pyramid_Controller : MonoBehaviour
     [Header("Pyramid Settings")]
     [SerializeField] float height;
     [SerializeField] float width;
+    [SerializeField] int stairs_count;
+
+    private void Update()
+    {
+        //Draw initial square
+        Debug.DrawLine(new Vector3(width / 2, 0, width / 2), new Vector3(width / 2, 0, -width / 2));
+        Debug.DrawLine(new Vector3(width / 2, 0, -width / 2), new Vector3(-width / 2, 0, -width / 2));
+        Debug.DrawLine(new Vector3(-width / 2, 0, -width / 2), new Vector3(-width / 2, 0, width / 2));
+        Debug.DrawLine(new Vector3(-width / 2, 0, width / 2), new Vector3(width / 2, 0, width / 2));
+
+        //
+        float step_height = height / stairs_count;
+        float width_step = width / stairs_count;
+
+        for (int i = 0; i < stairs_count; ++i)
+        {
+            Debug.DrawLine(new Vector3(width / 2 - width_step * i, step_height * i, width / 2 - width_step * i), new Vector3(width / 2 - width_step * i, step_height * i, -width / 2 + width_step * i));
+            Debug.DrawLine(new Vector3(width / 2 - width_step * i, step_height * i, -width / 2 + width_step * i), new Vector3(-width / 2 + width_step * i, step_height * i, -width / 2 + width_step * i));
+            Debug.DrawLine(new Vector3(-width / 2 + width_step * i, step_height * i, -width / 2 + width_step * i), new Vector3(-width / 2 + width_step * i, step_height * i, width / 2 - width_step * i));
+            Debug.DrawLine(new Vector3(-width / 2 + width_step * i, step_height * i, width / 2 - width_step * i), new Vector3(width / 2 - width_step * i, step_height * i, width / 2 - width_step * i));
+        }
+    }
 }
