c7fd4d1ebd8334637158ab2437224dde95511d2b
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-19 17:52:32 +0200

    Update Point_Symetry.cs

--- Assets/-Scripts/Geometry/Point_Symetry.cs
+++ Assets/-Scripts/Geometry/Point_Symetry.cs
@@ -16,14 +16,18 @@ public class Point_Symetry : MonoBehaviour
             Debug.DrawLine(new Vector3(vertexes[i].x, 0f, vertexes[i].y), new Vector3(vertexes[i + 1].x, 0f, vertexes[i + 1].y));
         }
 
-        if (bridge_shape) Debug.DrawLine(new Vector3(vertexes[0].x, 0f, vertexes[0].y), new Vector3(vertexes[vertexes.Count - 1].x, 0f, vertexes[vertexes.Count - 1].y));
-
-        //Rendering symetric shape using new vector = 2 * symetric point - original
+        //Rendering symetric shape using: new vector = 2 * symetric point - original
         for (int i = 0; i < vertexes.Count - 1; i++)
         {
             Debug.DrawLine(new Vector3((2 * point.position.x) - vertexes[i].x, 0f, (2 * point.position.z) - vertexes[i].y), new Vector3((2 * point.position.x) - vertexes[i+1].x, 0f, (2 * point.position.z) - vertexes[i+1].y));
         }
 
-        if (bridge_shape) Debug.DrawLine(new Vector3((2 * point.position.x) - vertexes[0].x, 0f, (2 * point.position.z) - vertexes[0].y), new Vector3((2 * point.position.x) - vertexes[vertexes.Count - 1].x, 0f, (2 * point.position.z) - vertexes[vertexes.Count - 1].y));
+        // Connecting first and last vertex in order to make a loop
+        if (bridge_shape)
+        {
+            Debug.DrawLine(new Vector3(vertexes[0].x, 0f, vertexes[0].y), new Vector3(vertexes[vertexes.Count - 1].x, 0f, vertexes[vertexes.Count - 1].y));
+
+            Debug.DrawLine(new Vector3((2 * point.position.x) - vertexes[0].x, 0f, (2 * point.position.z) - vertexes[0].y), new Vector3((2 * point.position.x) - vertexes[vertexes.Count - 1].x, 0f, (2 * point.position.z) - vertexes[vertexes.Count - 1].y));
+        }
     }
 }
