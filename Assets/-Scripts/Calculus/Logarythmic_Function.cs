5159b529b8c8b0bf2f91e52522b45e427b0098d2
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-19 20:03:33 +0200

    Update Logarythmic_Function.cs

--- Assets/-Scripts/Calculus/Logarythmic_Function.cs
+++ Assets/-Scripts/Calculus/Logarythmic_Function.cs
@@ -24,10 +24,13 @@ public class Logarythmic_Function : MonoBehaviour
 
     private void Update()
     {
+        // Avoiding getting an undefined value
         if (a == 0) return;
 
+        // Setting a to Euler's Number
         if (eulers) a = 2.71828f;
 
+        // Calculating this function with it's limit
         for (float i = resolution; i < limit; i += resolution)
         {
             if (i == 0) continue;
@@ -35,6 +38,7 @@ public class Logarythmic_Function : MonoBehaviour
             vectors.Add(new Vector3(i, 0, Mathf.Log(i, a)));
         }
 
+        // Plotting the function
         for (int i = 0; i < vectors.Count - 1; i++)
         {
             if (vectors[i].x < 0 && vectors[i].x > 0) continue;
@@ -49,14 +53,18 @@ public class Logarythmic_Function : MonoBehaviour
 
     private void Visualise_Examined_Number()
     {
+        // Calculating evaluation for the analyzed number
         evaluation = Mathf.Log(number, a);
 
+        // Visualising analyzed number value on Y axis
         if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.red);
         else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.red);
 
+        // Visualising analyzed number value on X axis
         if (evaluation >= 0) Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.blue);
         else Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, 0.5f), Color.blue);
 
+        // Rendering lines that point evaluation on the graph
         Debug.DrawLine(new Vector3(number, 0, 0), new Vector3(number, 0, evaluation), Color.green);
         Debug.DrawLine(new Vector3(0, 0, evaluation), new Vector3(number, 0, evaluation), Color.green);
     }
