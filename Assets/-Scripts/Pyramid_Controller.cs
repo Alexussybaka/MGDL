28093025ad6c0a3eb0feccfa6c125c85d63b956f
Alexussybaka <alex10.svoboda@gmail.com>
2025-06-28 11:13:04 +0200

    Pyramid & Demo work

--- Assets/-Scripts/Pyramid_Controller.cs
+++ Assets/-Scripts/Pyramid_Controller.cs
@@ -28,5 +28,18 @@ public class Pyramid_Controller : MonoBehaviour
             Debug.DrawLine(new Vector3(-width / 2 + width_step * i, step_height * i, -width / 2 + width_step * i), new Vector3(-width / 2 + width_step * i, step_height * i, width / 2 - width_step * i));
             Debug.DrawLine(new Vector3(-width / 2 + width_step * i, step_height * i, width / 2 - width_step * i), new Vector3(width / 2 - width_step * i, step_height * i, width / 2 - width_step * i));
         }
+
+        for (int j = 1; j < stairs_count + 1; j++)
+        {
+            Debug.DrawLine(new Vector3(width / 2 - width_step * (j - 1), step_height * j, width / 2 - width_step * (j - 1)), new Vector3(width / 2 - width_step * (j - 1), step_height * j, -width / 2 + width_step * (j - 1)));
+            Debug.DrawLine(new Vector3(width / 2 - width_step * (j - 1), step_height * j, -width / 2 + width_step * (j - 1)), new Vector3(-width / 2 + width_step * (j - 1), step_height * j, -width / 2 + width_step * (j - 1)));
+            Debug.DrawLine(new Vector3(-width / 2 + width_step * (j - 1), step_height * j, -width / 2 + width_step * (j - 1)), new Vector3(-width / 2 + width_step * (j - 1), step_height * j, width / 2 - width_step * (j - 1)));
+            Debug.DrawLine(new Vector3(-width / 2 + width_step * (j - 1), step_height * j, width / 2 - width_step * (j - 1)), new Vector3(width / 2 - width_step * (j - 1), step_height * j, width / 2 - width_step * (j - 1)));
+        }
+
+        for (int f = 0; f < stairs_count; f++)
+        {
+            Debug.DrawLine(new Vector3(), new Vector3());
+        }
     }
 }
