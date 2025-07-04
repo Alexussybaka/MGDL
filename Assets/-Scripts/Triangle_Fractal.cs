567f875075237c26ca1f832f632be91e9e7e7d1f
Alexussybaka <alex10.svoboda@gmail.com>
2025-07-04 22:54:14 +0200

    Update Triangle_Fractal.cs

--- Assets/-Scripts/Triangle_Fractal.cs
+++ Assets/-Scripts/Triangle_Fractal.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 public class Triangle_Fractal : MonoBehaviour
 {
     [SerializeField] int stage;
-    [SerializeField] float scale;
+    [SerializeField] float size;
 
     private void Update()
     {
-        
+        float basic_triangle_length = size / Mathf.Pow(2, stage - 1);
+        float basic_triangle_height = Mathf.Sin(Mathf.PI / 3) * basic_triangle_length;
+
+        for (int i = 0; i < Mathf.Pow(2, stage - 1); i++)
+        {
+            Debug.DrawLine(new Vector3(i * basic_triangle_length, 0, 0), new Vector3((i + 1) * basic_triangle_length, 0, 0));
+            
+        }
     }
 }
