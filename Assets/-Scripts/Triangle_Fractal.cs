259373979de8ae6c44c950d011a087b767f0057a
Alexussybaka <alex10.svoboda@gmail.com>
2025-07-05 12:37:06 +0200

    Update Triangle_Fractal.cs

--- Assets/-Scripts/Triangle_Fractal.cs
+++ Assets/-Scripts/Triangle_Fractal.cs
@@ -12,14 +12,20 @@ public class Triangle_Fractal : MonoBehaviour
     {
         float basic_triangle_length = size / Mathf.Pow(2, stage - 1);
         float basic_triangle_height = Mathf.Sin(Mathf.PI / 3) * basic_triangle_length;
+        float basic_triangle_offset = Mathf.Cos(Mathf.PI / 3) * basic_triangle_length;
         
         for (int j = 0; j < Mathf.Pow(2, stage - 1); j++)
         {
             for (int i = 0; i < Mathf.Pow(2, stage - 1); i++)
             {
-                Debug.DrawLine(new Vector3(i * basic_triangle_length, 0, 0), new Vector3((i + 1) * basic_triangle_length, 0, 0));
-                Debug.DrawLine(new Vector3(i * basic_triangle_length, 0, 0), new Vector3((i * basic_triangle_length) + (basic_triangle_length / 2), 0, basic_triangle_height));
-                Debug.DrawLine(new Vector3((i + 1) * basic_triangle_length, 0, 0), new Vector3((i * basic_triangle_length) + (basic_triangle_length / 2), 0, basic_triangle_height));
+                Debug.DrawLine(new Vector3(i * basic_triangle_length, 0, j * basic_triangle_height), 
+                    new Vector3((i + 1) * basic_triangle_length, 0, j * basic_triangle_height));
+                
+                Debug.DrawLine(new Vector3(i * basic_triangle_length, 0, j * basic_triangle_height), 
+                    new Vector3((i * basic_triangle_length) + (basic_triangle_length / 2), 0, (j + 1) * basic_triangle_height));
+                
+                Debug.DrawLine(new Vector3((i + 1) * basic_triangle_length, 0, j * basic_triangle_height), 
+                    new Vector3((i * basic_triangle_length) + (basic_triangle_length / 2), 0, (j + 1) * basic_triangle_height));
             }
         }
     }
