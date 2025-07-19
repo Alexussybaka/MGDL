05f094973c1b5c8ed2eece26ec331d2665391b0b
Alexussybaka <alex10.svoboda@gmail.com>
2025-07-19 13:01:35 +0200

    Added Hyperbolic Tangent and Cotangent Functions

---
+++ Assets/-Scripts/Calculus/Trigonometry/Hyperbolic_Tangent_Function.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hyperbolic_Tangent_Function : MonoBehaviour
+{
+    [Header("Graph Settings")]
+    [SerializeField] bool show_axis;
+    [SerializeField] float limit;
+    [Range(0.05f, 5f)]
+    [SerializeField] float resolution;
+
+    [Space]
+    [Header("Analyzed Number")]
+    [SerializeField] float number;
+    [SerializeField] bool eulers;
+    [Space]
+    [SerializeField] float evaluation;
+
+    private List<Vector3> vectors = new List<Vector3>();
+
+    const float e = 2.71828f;
+
+    private void Update()
+    {
+        // Clamping limit to always be a positive number
+        if (limit < 0) limit = 0;
+
+        // Calculating this function with it's limit
+        for (float i = -limit; i < limit; i += resolution)
+        {
+            float eval = (Mathf.Pow(e, 2 * i) - 1) / (Mathf.Pow(e, 2 * i) + 1);
+            if (Mathf.Abs(eval) <= limit) vectors.Add(new Vector3(i, 0f, eval));
+        }
+
+        // Plotting the function
+        for (int i = 0; i < vectors.Count - 1; i++)
+        {
+            Debug.DrawLine(vectors[i], vectors[i + 1]);
+        }
+
+        vectors.Clear();
+
+        // Additional features
+        Visualise_Examined_Number();
+        Show_Axis();
+    }
+
+    public void Visualise_Examined_Number()
+    {
+        if (eulers)
+        {
+            // Calculating evaluation for the analyzed number
+            evaluation = (Mathf.Pow(e, 2 * number * e) - 1) / (Mathf.Pow(e, 2 * number * e) + 1);
+
+            // Visualising analyzed number value on Y axis
+            if (evaluation >= 0) Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, -0.5f), Color.red);
+            else Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, 0.5f), Color.red);
+
+            // Visualising analyzed number value on X axis
+            if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.blue);
+            else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.blue);
+
+            // Rendering lines that point evaluation on the graph
+            Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, 0f), new Vector3(number * Mathf.PI, 0f, evaluation), Color.green);
+            Debug.DrawLine(new Vector3(number * Mathf.PI, 0f, evaluation), new Vector3(0f, 0f, evaluation), Color.green);
+        }
+        else
+        {
+            // Calculating evaluation for the analyzed number
+            evaluation = (Mathf.Pow(e, 2 * number) - 1) / (Mathf.Pow(e, 2 * number) + 1);
+
+            // Visualising analyzed number value on Y axis
+            if (evaluation >= 0) Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.red);
+            else Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, 0.5f), Color.red);
+
+            // Visualising analyzed number value on X axis
+            if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.blue);
+            else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.blue);
+
+            // Rendering lines that point evaluation on the graph
+            Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, evaluation), Color.green);
+            Debug.DrawLine(new Vector3(number, 0f, evaluation), new Vector3(0f, 0f, evaluation), Color.green);
+        }
+    }
+
+    public void Show_Axis()
+    {
+        if (show_axis)
+        {
+            // Plotting X and Y axis
+            Debug.DrawLine(new Vector3(-limit, 0, 0), new Vector3(limit, 0, 0));
+            Debug.DrawLine(new Vector3(0, 0, -limit), new Vector3(0, 0, limit));
+        }
+    }
+}
