b3cc868c881a38449520a013cb626056b20b6589
Alexussybaka <alex10.svoboda@gmail.com>
2025-08-26 21:47:25 +0200

    Quadratic Fubction Update

--- Assets/-Scripts/Calculus/Quadratic_Function.cs
+++ Assets/-Scripts/Calculus/Quadratic_Function.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Quadratic_Function : MonoBehaviour
 {
     [Header("Graph Settings")]
-    [SerializeField] bool horizontal;
+    [Header("y = ax^2 + bx + c")]
+    [SerializeField] float a;
+    [SerializeField] float b;
+    [SerializeField] float c;
+    [Space]
     [SerializeField] bool show_axis;
     [SerializeField] float limit;
     [Range(0.05f, 5f)]
@@ -24,17 +28,10 @@ public class Quadratic_Function : MonoBehaviour
     public void Update()
     {
         // Created all negative points to draw line using them.
-        for (float i = -limit; i < 0; i += resolution)
+        for (float i = -limit; i < limit; i += resolution)
         {
-            if (horizontal) vectors.Add(new Vector3(i, 0f, Mathf.Pow(i, 2)));
-            else vectors.Add(new Vector3(Mathf.Pow(i, 2), 0f, i));
-        }
-
-        // Created all positive points to draw line using them.
-        for (float i = 0; i < limit; i += resolution)
-        {
-            if(horizontal) vectors.Add(new Vector3(i, 0f, Mathf.Pow(i, 2)));
-            else vectors.Add(new Vector3(Mathf.Pow(i, 2), 0f, i));
+            float eval = (a * Mathf.Pow(i, 2)) + (b * i) + c;
+            vectors.Add(new Vector3(i, 0f, eval));
         }
         
         // Drawing all lines
@@ -53,75 +50,39 @@ public class Quadratic_Function : MonoBehaviour
 
     public void Visualise_Examined_Number()
     {
-        if (horizontal)
+        
+
+        if (is_power)
         {
-            if (is_power)
-            {
-                // Visualising analyzed number value on Y axis
-                Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(-0.5f, 0f, number), Color.red);
+            // Calculating evaluation for the analyzed number
+            evaluation = Mathf.Sqrt((a * Mathf.Pow(number, 2)) + (b * number) + c);
+            
+            // Visualising analyzed number value on Y axis
+            Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(-0.5f, 0f, number), Color.red);
 
-                // Visualising analyzed number value on X axis
-                if (number >= 0) Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, 0f), new Vector3(Mathf.Sqrt(number), 0f, -0.5f), Color.blue);
-                else Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, 0f), new Vector3(Mathf.Sqrt(number), 0f, 0.5f), Color.blue);
+            // Visualising analyzed number value on X axis
+            if (number >= 0) Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, 0f), new Vector3(Mathf.Sqrt(number), 0f, -0.5f), Color.blue);
+            else Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, 0f), new Vector3(Mathf.Sqrt(number), 0f, 0.5f), Color.blue);
 
-                // Rendering lines that point evaluation on the graph
-                Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(Mathf.Sqrt(number), 0f, number), Color.green);
-                Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, number), new Vector3(Mathf.Sqrt(number), 0f, 0f), Color.green);
-
-                // Calculating evaluation for the analyzed number
-                evaluation = Mathf.Sqrt(number);
-            }
-            else
-            {
-                // Visualising analyzed number value on Y axis
-                Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.red);
-
-                // Visualising analyzed number value on X axis
-                if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, Mathf.Pow(number, 2)), new Vector3(-0.5f, 0f, Mathf.Pow(number, 2)), Color.blue);
-                else Debug.DrawLine(new Vector3(0f, 0f, Mathf.Pow(number, 2)), new Vector3(0.5f, 0f, Mathf.Pow(number, 2)), Color.blue);
-
-                // Rendering lines that point evaluation on the graph
-                Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, Mathf.Pow(number, 2)), Color.green);
-                Debug.DrawLine(new Vector3(number, 0f, Mathf.Pow(number, 2)), new Vector3(0f, 0f, Mathf.Pow(number, 2)), Color.green);
-
-                // Calculating evaluation for the analyzed number
-                evaluation = Mathf.Pow(number, 2);
-            }
+            // Rendering lines that point evaluation on the graph
+            Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(Mathf.Sqrt(number), 0f, number), Color.green);
+            Debug.DrawLine(new Vector3(Mathf.Sqrt(number), 0f, number), new Vector3(Mathf.Sqrt(number), 0f, 0f), Color.green);
         }
         else
         {
-            if (is_power)
-            {
-                // Visualising analyzed number value on Y axis
-                Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.red);
+            // Calculating evaluation for the analyzed number
+            evaluation = (a * Mathf.Pow(number, 2)) + (b * number) + c;
 
-                // Visualising analyzed number value on X axis
-                if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, Mathf.Sqrt(number)), new Vector3(-0.5f, 0f, Mathf.Sqrt(number)), Color.blue);
-                else Debug.DrawLine(new Vector3(0f, 0f, Mathf.Sqrt(number)), new Vector3(0.5f, 0f, Mathf.Sqrt(number)), Color.blue);
+            // Visualising analyzed number value on Y axis
+            Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, -0.5f), Color.red);
 
-                // Rendering lines that point evaluation on the graph
-                Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, Mathf.Sqrt(number)), Color.green);
-                Debug.DrawLine(new Vector3(number, 0f, Mathf.Sqrt(number)), new Vector3(0f, 0f, Mathf.Sqrt(number)), Color.green);
+            // Visualising analyzed number value on X axis
+            if (number >= 0) Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(-0.5f, 0f, evaluation), Color.blue);
+            else Debug.DrawLine(new Vector3(0f, 0f, evaluation), new Vector3(0.5f, 0f, evaluation), Color.blue);
 
-                // Calculating evaluation for the analyzed number
-                evaluation = Mathf.Sqrt(number);
-            }
-            else
-            {
-                // Visualising analyzed number value on Y axis
-                Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(-0.5f, 0f, number), Color.red);
-
-                // Visualising analyzed number value on X axis
-                if (number >= 0) Debug.DrawLine(new Vector3(Mathf.Pow(number, 2), 0f, 0f), new Vector3(Mathf.Pow(number, 2), 0f, -0.5f), Color.blue);
-                else Debug.DrawLine(new Vector3(Mathf.Pow(number, 2), 0f, 0f), new Vector3(Mathf.Pow(number, 2), 0f, 0.5f), Color.blue);
-
-                // Rendering lines that point evaluation on the graph
-                Debug.DrawLine(new Vector3(0f, 0f, number), new Vector3(Mathf.Pow(number, 2), 0f, number), Color.green);
-                Debug.DrawLine(new Vector3(Mathf.Pow(number, 2), 0f, number), new Vector3(Mathf.Pow(number, 2), 0f, 0f), Color.green);
-
-                // Calculating evaluation for the analyzed number
-                evaluation = Mathf.Pow(number, 2);
-            }
+            // Rendering lines that point evaluation on the graph
+            Debug.DrawLine(new Vector3(number, 0f, 0f), new Vector3(number, 0f, evaluation), Color.green);
+            Debug.DrawLine(new Vector3(number, 0f, evaluation), new Vector3(0f, 0f, evaluation), Color.green);
         }
     }
 
